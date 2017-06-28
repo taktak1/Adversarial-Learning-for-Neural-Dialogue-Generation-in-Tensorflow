85212cf4918f8feab7d0456ae1c02ea43825e3cc
Satoshi Bando <s.bando1023@gmail.com>
2017-06-28 17:06:37 +0900

    fixed

--- al_neural_dialogue_train.py
+++ al_neural_dialogue_train.py
@@ -327,6 +327,12 @@ def al_train():
             reward, _ = disc_step(sess, bucket_id, disc_model, train_query, train_answer, train_labels, forward_only=True)
             batch_reward += reward / gen_config.steps_per_checkpoint
 
+            reward, _ = disc_step(sess, bucket_id, disc_model, neg_train_query, neg_train_answer,  neg_train_labels, forward_only=True)
+            batch_reward += reward / gen_config.steps_per_checkpoint
+
+            reward, _ = disc_step(sess, bucket_id, disc_model, null_train_query, null_train_answer, null_train_labels, forward_only=True)
+            batch_reward += reward / gen_config.steps_per_checkpoint
+
             # 4.Update G on (X, ^Y ) using reward r
             gan_adjusted_loss, gen_step_loss, _ =gen_model.step(sess, encoder, decoder, weights, bucket_id, forward_only=False,
                                            reward=reward, up_reward=True, debug=True)
